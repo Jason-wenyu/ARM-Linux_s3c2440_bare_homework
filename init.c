c9cffce8bde6675f47c3377e4d15f5825b063902
Jason Liu <1730266680@qq.com>
2019-10-10 00:58:40 +0800

    add nand_flash.c

--- init.c
+++ init.c
@@ -15,13 +15,49 @@ void sdram_init(void)
 	MRSRB7   = 0x20;
 }
 
+int isBootFromNorFlash(void)
+{
+    volatile int *p = (volatile *)0;
+    int val;
+
+    val = *p;
+    *p = 0x12345678;
+    if(*p == 0x12345678)
+    {
+        /*写成功，是nand启动*/
+        *p = val;
+        return 0;
+    }
+    else
+    {
+        /*NOR Flash启动*/
+        return 1;
+    }
+    
+}
+
 void copy2sdram(void)
 {
     	/* 要从lds文件中获得 __code_start, __bss_start
      * 然后从0地址把数据复制到__code_start
      */
     extern int __code_start,__bss_start;
-    
+    volatile unsigned int *dest = (volatile int *)&__code_start;
+    volatile unsigned int *end = (volatile int *)&__bss_start;
+    volatile unsigned int *src = (volatile int *)0;
+    unsigned int len = (unsigned int)(&__code_start) -(unsigned int)(&__bss_start)
+    if(isBootFromNorFlash())
+    {
+        while(dest < end)
+        {
+            *dest++ = *src++;
+        }
+        else
+        {
+            nand_init();
+            nand_read((unsigned int)src, dest, len);
+        }
+    }
     
 }
 
